26d2c3f3178f2bce6b2bacd1b9bf7eec4632224f
koharakouki <ko.kohara02@gmail.com>
2020-06-07 11:24:45 +0000

    [Add]認証ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+devise_for :users, controllers: {
+  sessions: 'devise/users/sessions',
+  registrations: 'devise/users/registrations',
+  passwords: 'devise/users/passwords',
+}
+
 root to: 'home#top'
 
 end
